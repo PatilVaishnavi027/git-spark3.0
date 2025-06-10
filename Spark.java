7276ccc391e8f036dcda908d8bb3619a94717232
vaishnavi <patilvaishnavi2710@gmail.com>
2025-06-10 21:43:20 +0530

    file modified

--- Spark.java
+++ Spark.java
@@ -4,6 +4,7 @@ public class Spark {
         int res= sum(1,2);
         System.out.println(res);
         notifyUser();
+        auth();
     }
     public static int sum(int a,int b)
     {
@@ -13,5 +14,9 @@ public class Spark {
 
     {
         System.out.println("hello user");
+    }
+    public static void auth()
+    {
+        System.out.println("hii buddy");
     }
 }
