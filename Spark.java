2cf9358c80312cadae8ef5855464f6a3084bc95e
vaishnavi <patilvaishnavi2710@gmail.com>
2025-06-10 21:12:39 +0530

    notification service add

--- Spark.java
+++ Spark.java
@@ -3,9 +3,15 @@ public class Spark {
         System.out.println("hello");
         int res= sum(1,2);
         System.out.println(res);
+        notifyUser();
     }
     public static int sum(int a,int b)
     {
         return a+b;
+    }
+    public static void notifyUser()
+
+    {
+        System.out.println("hello user");
     }
 }
