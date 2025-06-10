a49dc29b9d7ffd2539f0b29971c82fd97de265b9
vaishnavi <patilvaishnavi2710@gmail.com>
2025-06-10 21:02:22 +0530

    file modified

--- Spark.java
+++ Spark.java
@@ -1,6 +1,11 @@
 public class Spark {
     public static void main(String[] args) {
         System.out.println("hello");
+        int res= sum(1,2);
+        System.out.println(res);
     }
-    
+    public static int sum(int a,int b)
+    {
+        return a+b;
+    }
 }
